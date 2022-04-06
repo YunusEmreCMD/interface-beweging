9c73a657e7e5ebe2766556c79a4bd2e497679f54
Yunusemre Alkan <74190945+YunusEmreCMD@users.noreply.github.com>
2022-04-06 13:41:20 +0200

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,9 @@
+// console.log('abc');
+
+// var knoppenContainer = document.getElementsByClassName("knoppen-container");
+
+// var bekijkKnop = document.getElementsByClassName("knoppen-container h2");
+
+// bekijkKnop.addEventListener("click", function() {
+// 	knoppenContainer.classList.toggle("toonKnoppen");
+// });
